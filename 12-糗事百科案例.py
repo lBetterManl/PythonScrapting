b70bbe3dc49d0c1d0ea9d976555d4bbe7dad12c3
lBetterManl <yhc8023tm@foxmail.com>
2018-09-02 16:57:21 +0800

    Python爬虫

--- 12-糗事百科案例.py
+++ 12-糗事百科案例.py
@@ -2,4 +2,18 @@ import requests
 from fake_useragent import UserAgent
 import re
 
-url = ""
+url = "https://www.qiushibaike.com/text/page/1/"
+headers = {
+    "User-Agent": UserAgent().random
+}
+# 关闭警告
+requests.packages.urllib3.disable_warnings()
+# 构造请求
+response = requests.get(url, headers=headers, verify=False)
+info = response.text
+# print(info)
+infos = re.findall(r'<div class="content">\s*<span>\s*(.+)\s*</span>', info)
+# print(infos)
+with open("duanzi.txt", 'a', encoding="utf-8") as f:
+    for duanzi in infos:
+        f.write(re.sub(r'<.+.>', "\n", duanzi)+"\n\n\n")
